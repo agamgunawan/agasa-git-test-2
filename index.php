3f04477fc61f529198a7a0b54663574ea72922cb
agamgunawan <agasaaa23@gmail.com>
2024-03-15 10:55:20 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
     <body>
         <h1>Hello World!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam autem animi temporibus facilis quis officia blanditiis velit vel in quam? Deserunt vero ut iure aliquid officia, quibusdam dolore ad possimus?</p>
+        <ul>
+            <li>satu</li>
+            <li>dua</li>
+        </ul>
     </body>
 </html>
